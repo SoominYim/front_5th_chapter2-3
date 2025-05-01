d1373d7ca8982e79a72dff3dfb8ee0a3ee647ac9
SoominYim <soo15soo@kakao.com>
2025-05-01 21:16:44 +0900

    Chore: gh-pages 배포 스크립트 및 배포 환경 설정정

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,27 @@
-import { defineConfig } from "vite"
+import { defineConfig, Plugin } from "vite"
 import react from "@vitejs/plugin-react"
 
+
+// 배포 환경 시 api 요청 주소 변경
+function apiReplace(): Plugin { 
+  return {
+    name: "api-replace",
+    transform(code, id) {
+      if (id.endsWith('.ts') || id.endsWith('.js')) { // JS/TS 파일만 처리
+        return code.replace(/(["'`])\/api/g, `$1https://dummyjson.com`);
+      }
+    },
+  }
+}
+
+
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
+export default defineConfig(({ command }) => {
+  const base = command === "build" ? "/front_5th_chapter2-3/" : "/";
+  return {
+    base,
+    plugins: [react(), apiReplace()],
+    server: {
     proxy: {
       "/api": {
         // target: 'https://jsonplaceholder.typicode.com',
@@ -13,5 +30,6 @@ export default defineConfig({
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
-  },
+    },
+  }
 })
